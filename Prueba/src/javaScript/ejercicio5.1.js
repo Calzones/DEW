dd71bcf6a34e5510342c664001a3b6268f82531b
FabricioRojas <peque_arinaga@hotmail.es>
2016-01-18 20:40:49 +0000

    Tarde 18/1/16

--- Prueba/src/javaScript/ejercicio5.1.js
+++ Prueba/src/javaScript/ejercicio5.1.js
@@ -1,13 +1,36 @@
-var caca;
-
 function añadir() {
-	if(/^(?!.*\.{2})(?=[^\.])[\w\-\.]+@[\w\-]+(\.[A-z]+)+/.test(document.getElementById("email").value)){
-		return caca = [document.getElementById("apellidos").value,document.getElementById("nombre").value,document.getElementById("email").value];
-	}else{
+	if (/^(?!.*\.{2})(?=[^\.])[\w\-\.]+@[\w\-]+(\.[A-z]+)+/.test(document
+			.getElementById("email").value)) {
+		document.getElementById("textarea").value = document
+				.getElementById("apellidos").value
+				+ document.getElementById("nombre").value
+				+ document.getElementById("email").value;
+	} else {
 		alert("Correo no válido");
 	}
 }
-
 function visualizar() {
-	alert("Nombre: " + caca[1] + "\nApellidos: " + caca[0] + "\nCorreo electronico: " + caca[2]);
+	var sexo = document.getElementsByName("sexo");
+	var resultado = "";
+	for (i = 0; i < sexo.length; i++) {
+		if (sexo[i].checked){
+			resultado = sexo[i].value;
+		}
+	}
+	var informacion = document.getElementsByName("informacion");
+	var resultado1 = "";
+	for (i = 0; i < informacion.length; i++) {
+		if (informacion[i].checked){
+			resultado1 += informacion[i].value + " ";
+		}
+	}
+	alert("Nombre: " + document.getElementById("nombre").value
+			+ "\nApellidos: " + document.getElementById("apellidos").value
+			+ "\nCorreo electronico: " + document.getElementById("email").value
+			+ "\nSexo: " + resultado
+			+ "\nOrigen: " + resultado1
+			+ "\nIntereses: " + document.getElementById("select").value);
+}
+function eliminar() {
+	document.getElementById("textarea").value = "";
 }
--- Prueba/src/javaScript/ejercicio5.2.js
+++ Prueba/src/javaScript/ejercicio5.2.js
@@ -1,15 +1,88 @@
-function validar(){
-	if(id("nombre") && id("apellido1") && id("edad") && id("matricula") && id("numTarjeta") && id("mesExp") && id("numExp") && id("apellido2") && id("fechaPermiso") && id("importe") && id("añoExp")){
-		alert("guien");
-	}else{
-		alert("mal");
+function validar() {
+	if (vagia("nombre") && vagia("apellido1") && vagia("edad")
+			&& vagia("matricula") && vagia("numTarjeta") && vagia("mesExp")
+			&& vagia("numExp") && vagia("apellido2") && vagia("fechaPermiso")
+			&& vagia("importe") && vagia("añoExp")) {
+		alert("Formulario validado");
+	} else {
+		alert("Faltan campos por completar");
 	}
 }
-//[\d]{4}[^a-z^\dAEIOU]{3} matricula
-function id(elemento){
-	if(document.getElementById(elemento).value != ""){
+// [\d]{4}[^a-z^\dAEIOU]{3}
+function noNum(elemento) {
+	if (!/^[A-z]+$/.test(document.getElementById(elemento).value)) {
+		alert("No se permiten numeros");
+	}
+}
+function numExp(elemento) {
+	if (vagia(elemento)) {
+		if (!(document.getElementById(elemento).value <= 349999999999 && document.getElementById(elemento).value >= 340000000000)) {
+			alert("No esta dentro del rango: 340000000000-349999999999");
+		}
+	} else {
+		alert("Falta completar este campo");
+	}
+}
+function vagia(elemento) {
+	if (document.getElementById(elemento).value != "") {
 		return true;
-	}else{
+	} else {
 		return false;
 	}
 }
+function edad(elemento) {
+	if (vagia(elemento)) {
+		if (!(document.getElementById(elemento).value <= 120 && document.getElementById(elemento).value >= 18)) {
+			alert("No esta dentro del rango: 18-120");
+		}
+	} else {
+		alert("Falta completar este campo");
+	}
+}
+function fecha(elemento){
+	var cadenaEntera = document.getElementById(elemento).value; 
+	var separada = cadenaEntera.split("/");
+	var buscar = cadenaEntera.search("/");
+	var dia = separada[0];
+	var mes = separada[1];
+	var año = separada[2];
+	if(buscar == 2){
+		if(dia.length == 2 && mes.length == 2 && año.length == 4){
+			alert(dia.search("0"));
+		}else{
+			alert("Formato de fecha erroneo (dd/mm/aaaa)");
+		}
+	}else{
+		alert("Formato de fecha erroneo (dd/mm/aaaa)");
+	}
+}
+function dia(dia){
+	alert(dia.search("0"));
+	if(dia.search("0") > 0){
+		alert(dia.split("0")[1]);
+	}
+}
+function mes(){
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
