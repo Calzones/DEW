03968650d003755cfd920ac4450966e8f615161c
FabricioRojas <peque_arinaga@hotmail.es>
2016-01-31 18:45:07 +0000

    Tarde	31/1/16

--- Prueba/src/javaScript/ejercicio6.2.js
+++ Prueba/src/javaScript/ejercicio6.2.js
@@ -1,9 +1,1 @@
-function huirMuyFuerte(){
-	var d = document.getElementById("boton"); 
-	d.style.margin = numeroAleatorio() + "px";
-}
 
-function numeroAleatorio(){
-	var numero = Math.round(Math.random()*400);
-	return numero;
-}
